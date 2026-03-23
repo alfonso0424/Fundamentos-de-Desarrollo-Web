ac0e475fcf70705ca0fb6cf67b33ff33ea608a55
alfonso0424 <alfonso14705@gmail.com>
2026-03-23 17:22:05 -0600

    tareas

---
+++ Parcial 2/Tareas/Tarea 6 Menu Responsivo Clase/main.js
@@ -0,0 +1,14 @@
+const nav = document.getElementById("nav")
+
+function toogleMenu()
+{
+    // if(menubar.classList.contains("open"))
+    // {
+    //     menubar.classList.remove("open")
+    // }
+    // else{
+    //     menubar.classList.add("open")
+    // }
+
+    menubar.classList.toggle("open");
+}
